0261956a6e5deb43f05bceeee7f612e1feb78c81
Alex <alexandre.tullot@gmail.com>
2018-01-30 09:19:36 +0100

    renome les fichier

--- divertissement.php
+++ divertissement.php
@@ -2,16 +2,16 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Forum - CatchLife™</title>
 </head>
 <body>
 	<div class="header">
 		<ul>
-			<li><img src="arrow4.png" alt=""><a href="main.html"> Accueil</a></li>
-			<li><img src="arrow4.png" alt=""><a href="Forum.html"> Forum</a></li>
-			<li><img src="arrow4.png" alt=""><a href="divertissement.html"> Divertissement</a></li>
-			<li><img src="arrow4.png" alt=""><a href=""> Déconnexion</a></li>
+			<li><a href="index.php"><img src="arrow4.png" alt=""> Accueil</a></li>
+			<li><a href="Forum.php"><img src="arrow4.png" alt=""> Forum</a></li>
+			<li><a href="divertissement.php"><img src="arrow4.png" alt=""> Divertissement</a></li>
+			<li><a href=""><img src="arrow4.png" alt=""> Déconnexion</a></li>
 		</ul>
 	</div>
 
