98909387831ee9c3be4e29e6d5e85f6975f6aa76
Alex <alexandre.tullot@gmail.com>
2018-01-30 09:25:53 +0100

    Ajout du menu.php pour faciliter le changement de la barre de navigation

--- divertissement.php
+++ divertissement.php
@@ -6,14 +6,8 @@
 	<title>Forum - CatchLife™</title>
 </head>
 <body>
-	<div class="header">
-		<ul>
-			<li><a href="index.php"><img src="arrow4.png" alt=""> Accueil</a></li>
-			<li><a href="Forum.php"><img src="arrow4.png" alt=""> Forum</a></li>
-			<li><a href="divertissement.php"><img src="arrow4.png" alt=""> Divertissement</a></li>
-			<li><a href=""><img src="arrow4.png" alt=""> Déconnexion</a></li>
-		</ul>
-	</div>
+	
+	<?php include('menu.php'); ?>
 
 	<div class="titlesection">
 		<h1>Divertissement</h1>
---
+++ menu.php
@@ -0,0 +1,10 @@
+	<nav>
+		<div class="header">
+			<ul>
+                 <li><a href="index.php"><img src="arrow4.png" alt=""> Accueil</a></li>
+			     <li><a href="Forum.php"><img src="arrow4.png" alt=""> Forum</a></li>
+			     <li><a href="divertissement.php"><img src="arrow4.png" alt=""> Divertissement</a></li>
+			     <li><a href=""><img src="arrow4.png" alt=""> Déconnexion</a></li>
+			 </ul>
+        </div>
+	</nav>
