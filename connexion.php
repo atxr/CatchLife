f874eaca958d92ba069c78e29921b21fc296cb58
Alex <alexandre.tullot@gmail.com>
2018-01-30 09:35:50 +0100

    ajout de connection.php

---
+++ connexion.php
@@ -0,0 +1,34 @@
+<?php
+session_start(); // On démarre la session AVANT toute chose
+$_SESSION['login'] = $_POST['login'];
+$_SESSION['pass'] = $_POST['pass'];
+?>
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta charset="utf-8">
+<head>
+	<title>CatchLife™</title>
+</head>
+
+
+<body>
+
+	<?php include('menu.php'); ?> 
+
+	<div class="labelconnexion">
+		<?php 
+			if (isset($_POST['login']) AND htmlspecialchars($_SESSION['login']) AND $_POST['pass'] == '123456789')
+			{
+				echo '<p>Vous êtes bien connecté en tant que "' . htmlspecialchars($_SESSION['login']) .'" !</br>Vous pouvez vous connecter au forum..</p>';
+			}
+			elseif ($_POST != '123456789')
+			{
+				echo '<p>Impossible de se connecter, veuillez vérifier votre utilisateur ou votre mot de passe!</p>';
+			}
+		?>
+	</div>
+</body>
+
+
+</html>
